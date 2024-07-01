5df7fd094223f44f064509eb06d32dbf877d853c
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-07-01 14:33:22 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,1 +1,32 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import joblib
 
+def main():
+    with st.sidebar:
+        selected = option_menu(
+            "Main Menu",
+            ["Home", "Klasifikasi Naive Bayes", "Topic Modelling", "Uji Coba"],
+            icons=['house', 'table', 'boxes', 'boxes', 'check2-circle'],
+            menu_icon="cast",
+            default_index=1,
+            orientation='vertical')
+    
+    if selected == 'Home':
+    
+    elif selected == 'Klasifikasi Naive Bayes':
+            
+    elif selected == 'Topic Modelling':
+        
+    elif selected == 'Uji Coba':
+ 
+if __name__ == "__main__":
+    main()
